e6ec689a3b109150f923d090b6a7bb5845c9b750
Apostolos Siokas <apostolossiokas@gmail.com>
2016-08-02 09:08:15 +0300

    Checks before routes added

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -4,26 +4,29 @@ use Siokas\Laryr\Laryr;
 
 $path = Config::get('laryr.path');
 
-// Create an instance of the Laryr object passing the path of the yml file
-$laryr = new Laryr($path);
+if ($path) {
 
-// Get the routes from the yaml file
-$routes = $laryr->getRoutesFromYaml();
+    $laryr = new Laryr($path);
+    $routes = $laryr->getRoutesFromYaml();
 
-// Run through all routes
-foreach ($routes as $route) {
+    if ($routes) {
 
-    if (isset($route['route']) && isset($route['controller'])) {
-        $method = isset($route['method']) ? $route['method'] : 'get';
-        $function = isset($route['function']) ? $route['function'] : 'index';
+        foreach ($routes as $route) {
 
-        Route::${"method"}($route['route'], [
-            'as' => isset($route['name']) ? $route['name'] : str_random(10),
-            'uses' => 'App\Http\Controllers\\' . $route['controller'] . '@' . $function,
-            'middleware' => isset($route['middleware']) ? $route['middleware'] : 'web',
-        ]);
-    } else {
-        abort(406, 'You have an error in the ' . $path . ' file. You must declare the route and the controller in the .yml file.');
+            if (isset($route['route']) && isset($route['controller'])) {
+                $method = isset($route['method']) ? $route['method'] : 'get';
+                $function = isset($route['function']) ? $route['function'] : 'index';
+
+                Route::${"method"}($route['route'], [
+                    'as' => isset($route['name']) ? $route['name'] : str_random(10),
+                    'uses' => 'App\Http\Controllers\\' . $route['controller'] . '@' . $function,
+                    'middleware' => isset($route['middleware']) ? $route['middleware'] : 'web',
+                ]);
+            } else {
+                abort(406, 'You have an error in the ' . $path . ' file. You must declare the route and the controller in the .yml file.');
+            }
+
+        }
     }
 
 }
